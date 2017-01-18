dd8275c965e1b27db0fd7bd6e92135e6a13ffa0a
lixiaojing <lixiaojing3@jd.com>
2017-01-18 13:40:21 +0800

    线程捕获异常

--- ddd-demo-infrastructure/src/main/java/ddd/demo/infrastructure/taskschedule/TaskScheduleConfig.java
+++ ddd-demo-infrastructure/src/main/java/ddd/demo/infrastructure/taskschedule/TaskScheduleConfig.java
@@ -2,6 +2,8 @@ package ddd.demo.infrastructure.taskschedule;
 
 
 import org.joda.time.DateTime;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.TaskScheduler;
@@ -10,16 +12,32 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 @Configuration
 public class TaskScheduleConfig {
 
+    private final Logger logger = LoggerFactory.getLogger(TaskScheduleConfig.class);
+
     @Bean
     public TaskScheduler taskScheduler() {
 
         ThreadPoolTaskScheduler taskScheduler = new ThreadPoolTaskScheduler();
         taskScheduler.setPoolSize(4);
+        taskScheduler.setErrorHandler(e -> {
+            logger.error(e.getMessage(), e);
+        });
 
         taskScheduler.initialize();
+
         taskScheduler.scheduleAtFixedRate(new Runnable() {
             @Override
             public void run() {
+
+                System.out.println("threadId=" + Thread.currentThread().getId());
+                try {
+                    Thread.sleep(7000);
+                    System.out.println("threadId=" + Thread.currentThread().getId() + "OK");
+
+                } catch (InterruptedException e) {
+
+                }
+
                 System.out.println(String.format("task ThreadPoolTaskScheduler time is %s", DateTime.now().toString()));
             }
         }, 5000);
--- ddd-demo-port/ddd-demo-api/src/main/java/ddd/demo/api/controllers/HomeController.java
+++ ddd-demo-port/ddd-demo-api/src/main/java/ddd/demo/api/controllers/HomeController.java
@@ -1,6 +1,8 @@
 package ddd.demo.api.controllers;
 
 import ddd.demo.application.order.OrderApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping("/")
 public class HomeController {
 
+    private final Logger logger = LoggerFactory.getLogger(HomeController.class);
     @Autowired
     private OrderApplication orderApplication;
 
@@ -19,4 +22,27 @@ public class HomeController {
     public String index() {
         return "你好，hello";
     }
+
+    @GetMapping("/thread")
+    @ResponseBody
+    public String ThreadTest() {
+
+        Thread thread = new Thread(() -> {
+            String n = "a1233";
+
+            int a = Integer.parseInt(n);
+
+        });
+        thread.setUncaughtExceptionHandler((t, e) -> {
+            logger.error(e.getMessage(), e);
+        });
+
+        thread.setDaemon(true);
+        thread.start();
+
+
+        return "ok";
+
+    }
+
 }
